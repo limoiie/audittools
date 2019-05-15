2eca67bc8bcf14a0e4cf43c1185361eda2b778f0
limoiie <limo.iie4@gmail.com>
2019-05-16 00:02:01 +0800

    add: Add rxcpp

--- ext/prepare_ext.cmake
+++ ext/prepare_ext.cmake
@@ -2,3 +2,4 @@ set(BINARY_EXT_DIR ${CMAKE_CURRENT_BINARY_DIR}/ext)
 file(MAKE_DIRECTORY ${BINARY_EXT_DIR})
 
 include(${AUDITTOOLS_DIR}/ext/google-test/add_googletest.cmake)
+include(${AUDITTOOLS_DIR}/ext/rxcpp/add_rxcpp.cmake)
